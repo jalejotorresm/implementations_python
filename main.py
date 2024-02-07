45af5a09d5cb283f614ba3e1fe9e94a607a713e6
Alejandro Torres <72758522+jalejotorresm@users.noreply.github.com>
2024-02-07 17:20:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from selector.selector import menu_seleccion
 
-print("\n------ Bienvenido a mi Coleccion de Miniprogramas en Python ------\n")
+print("\n------ Bienvenido a mi Coleccion de Miniprogramas en Python (con Virtualenv) ------\n")
 
 menu_seleccion()
